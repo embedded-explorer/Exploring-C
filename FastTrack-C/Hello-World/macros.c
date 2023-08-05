9f34d92d4496b7875c1d6a3debf54548da2c20b2
embedded-explorer <n.hrishikesh.prabhu@gmail.com>
2023-08-05 10:43:58 +0530

    Adding Exercise on Macros

--- FastTrack-C/Hello-World/macros.c
+++ FastTrack-C/Hello-World/macros.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-#define CAL(X) X*X
+// Object Like Macros
+#define SIZE 25
+#define MIN 5
+#define MAX 9
+
+// Function Like Macros
+#define MAX_ELEMENT(X, Y) X>Y ? X : Y
+#define SQUARE(X) X*X
 
 int main(){
-    // Text will be replaced as it is operation will not occur
-    printf("Result = %d\n", CAL(3+4)); // 3+4*3+4
+    // Text will be replaced as it is, be careful while passing arguments
+    printf("Result = %d\n", SQUARE(3+4)); // 3+4*3+4
+
+    printf("Max Value is %d\n", MAX_ELEMENT(MIN, MAX));
+    assert(SQUARE(5) == 25);
+
+    assert(SIZE == 25);
+
+// Conditional Macros
+#ifdef SIZE
+#undef SIZE
+#define SIZE 20
+    assert(SIZE == 20);
+#endif // SIZE
+
     return 0;
 }
